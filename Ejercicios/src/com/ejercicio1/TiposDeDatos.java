843fb7eb673ff601f0cac712903bcc4bb2302695
Sebastian Colombo <colombosebas@gmail.com>
2023-04-24 14:40:17 -0300

    Ejercicio1 OpenBootCamp

---
+++ Ejercicios/src/com/ejercicio1/TiposDeDatos.java
@@ -0,0 +1,33 @@
+package com.ejercicio1;
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+        //enteros
+        byte unByte = 1;
+        short unShort = 2;
+        int unEntero = 3;
+        long unLong = 4;
+
+        //decimales
+        float unfloat = 0.1f;
+        double unDouble = 0.2d;
+
+        //booleanos
+        boolean unBoolean = true;
+
+        //texto
+        char unCharacter = 'a';
+        String unString = "Ejercicio finalizado!";
+
+        System.out.println(unByte);
+        System.out.println(unShort);
+        System.out.println(unEntero);
+        System.out.println(unLong);
+        System.out.println(unfloat);
+        System.out.println(unDouble);
+        System.out.println(unBoolean);
+        System.out.println(unCharacter);
+        System.out.println(unString);
+    }
+
+}
