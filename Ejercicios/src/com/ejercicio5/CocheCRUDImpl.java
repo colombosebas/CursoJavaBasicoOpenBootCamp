2dd42da6ef6a4802a59e289941dc4d78ab83c97c
Sebastian Colombo <colombosebas@gmail.com>
2023-04-27 20:46:46 -0300

    Ejercicio 5, Interfaces

---
+++ Ejercicios/src/com/ejercicio5/CocheCRUDImpl.java
@@ -0,0 +1,15 @@
+package com.ejercicio5;
+
+public class CocheCRUDImpl implements CocheCRUD{
+
+    public void save(){
+        System.out.println("save");
+    }
+    public void findAll(){
+        System.out.println("findAll");
+    }
+    public void delete(){
+        System.out.println("delete");
+    }
+
+}
