c4c72517eb0a02892c70dbf60ef87c866e963287
Sebastian Colombo <colombosebas@gmail.com>
2023-04-27 20:47:22 -0300

    Ejercicio 5, Interfaces

--- Ejercicios/src/com/ejercicio5/Main.java
+++ Ejercicios/src/com/ejercicio5/Main.java
@@ -2,7 +2,7 @@ package com.ejercicio5;
 
 public class Main {
     public static void main(String[] args){
-        CocheCRUDImpl Coche = new CocheCRUDImpl();
+        CocheCRUD Coche = new CocheCRUDImpl();
         Coche.findAll();
         Coche.delete();
         Coche.save();
